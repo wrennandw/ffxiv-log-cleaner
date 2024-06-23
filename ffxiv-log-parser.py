86fd76b2d8f8d97e8884d2332496d3526ca6c0ae
sterpderp <wrennandw@gmail.com>
2024-06-23 00:29:57 -0500

    Add basic log cleaner

--- ffxiv-log-parser.py
+++ ffxiv-log-parser.py
@@ -1,12 +1,35 @@
 import os
 
+class EventLog:
+    def __init__(self, filename, log_data): 
+        self.filename = filename
+        self.log_data = log_data
+
+
 def loader():
     _logs = []
+    _log_objects = []
     _dir = os.listdir('./logs/raw')
     for e in _dir:
-        with open(f'./logs/raw/{e}', encoding='utf-8') as _text:
-            _logs_data = _text.read()
-            _logs.append([e, _logs_data])
-    return(_logs)
+        if e != 'directions.txt':
+            with open(f'./logs/raw/{e}', encoding='utf-8') as _text:
+                _logs_data = _text.read()
+                _logs.append([e, _logs_data])
+    
+    for e in _logs:
+        _log_objects.append(EventLog(e[0], e[1]))
+
+    return _log_objects
+
+
+def cleaner(data):
+        data.log_data = data.log_data.splitlines()
+        data.log_data = [e for e in data.log_data if e != '']
+        data.log_data = [e.split(' ', 1)[-1] for e in data.log_data]
 
 logs = loader()
+
+for e in logs:
+    cleaner(e)
+    print(e.filename)
+    print(e.log_data)
