a75f2396fbaacb56dd0e457955a2e16c09c8ffaf
sterpderp <wrennandw@gmail.com>
2024-06-23 00:38:07 -0500

    Clean up log object creation

--- ffxiv-log-parser.py
+++ ffxiv-log-parser.py
@@ -1,5 +1,6 @@
 import os
 
+# Basic log data structure
 class EventLog:
     def __init__(self, filename, log_data): 
         self.filename = filename
@@ -10,25 +11,32 @@ def loader():
     _logs = []
     _log_objects = []
     _dir = os.listdir('./logs/raw')
+    
+    # Load each log and instantiate it as an object
     for e in _dir:
         if e != 'directions.txt':
             with open(f'./logs/raw/{e}', encoding='utf-8') as _text:
                 _logs_data = _text.read()
-                _logs.append([e, _logs_data])
-    
-    for e in _logs:
-        _log_objects.append(EventLog(e[0], e[1]))
-
+                _log_objects.append(EventLog(e, _logs_data))
+                
     return _log_objects
 
 
 def cleaner(data):
+        # Remove newline characters
         data.log_data = data.log_data.splitlines()
+
+        # Remove empty strings
         data.log_data = [e for e in data.log_data if e != '']
+
+        # Remove class markers at start of each string
         data.log_data = [e.split(' ', 1)[-1] for e in data.log_data]
 
+
+# Load all logs in raw directory
 logs = loader()
 
+# Clean all logs
 for e in logs:
     cleaner(e)
     print(e.filename)
