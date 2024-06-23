34e61c81a12aff1cd6c2fa0fd4d5f026688021be
sterpderp <wrennandw@gmail.com>
2024-06-23 10:28:14 -0500

    Add server name cleaner

--- ffxiv-log-parser.py
+++ ffxiv-log-parser.py
@@ -8,7 +8,6 @@ class EventLog:
 
 
 def loader():
-    _logs = []
     _log_objects = []
     _dir = os.listdir('./logs/raw')
     
@@ -23,14 +22,41 @@ def loader():
 
 
 def cleaner(data):
-        # Remove newline characters
-        data.log_data = data.log_data.splitlines()
+    # Remove newline characters
+    data.log_data = data.log_data.splitlines()
 
-        # Remove empty strings
-        data.log_data = [e for e in data.log_data if e != '']
+    # Remove empty strings
+    data.log_data = [e for e in data.log_data if e != '']
 
-        # Remove class markers at start of each string
-        data.log_data = [e.split(' ', 1)[-1] for e in data.log_data]
+    # Remove class markers and hearts at start of each string
+    # LS/CWLS tags are left intact for clarity
+    _clean =[]
+
+    for e in data.log_data:
+        if e[0] != '[':
+            _clean.append(e.split(' ', 1)[-1])
+        else:
+            _clean.append(e)
+    
+    data.log_data = [e.replace('♥', '') for e in _clean]
+
+    # Remove server names
+    _lines = enumerate(data.log_data)
+    for e in _lines:
+        _bounds = []
+        
+        # Find the start and end of the surname, excluding the first letter
+        _bounds.append(e[1].find(' ')+2)
+        _bounds.append(e[1].find(' ', _bounds[0]+1)-1)
+        
+        # Check if there's a server in the surname and remove it
+        for i in range(_bounds[0], _bounds[1]):
+            if e[1][i].isupper():
+                print(f'{e[1][i:_bounds[1]]} {_bounds[0]}-{_bounds[1]}')
+                data.log_data[e[0]] = data.log_data[e[0]].replace(
+                    e[1][i:_bounds[1]],'')
+            break
+    
 
 
 # Load all logs in raw directory
